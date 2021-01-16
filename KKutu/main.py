4b942d17180227a91cf7761b6818414f7169916e
I-Dohyun-I <trouna43@gmail.com>
2021-01-16 23:17:53 +0900

    rebase

--- KKutu/main.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome('D:\chromedriver\chromedriver.exe')
-driver.get("https://kkutu.io/?server=0")
-
-def now():
-    element = driver.find_element_by_class_name("jjoDisplayBar")
-    return element.text[1] #:element.text.index("\n")
-
